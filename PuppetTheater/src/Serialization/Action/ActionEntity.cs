545f7336680dd035268c85ea0cbd1a73acd5aef5
95D <avos170@gmail.com>
2020-05-04 12:00:45 +0900

    Add ActionNode and ActionEntity classes

---
+++ PuppetTheater/src/Serialization/Action/ActionEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehaviorTree.Serialization.Action
+{
+    /// <summary>
+    /// This class data entity class for ActionNode.
+    /// </summary>
+    public class ActionEntity
+    {
+        public readonly string name;
+        public readonly string eventId;
+
+        public ActionEntity(string name, string eventId)
+        {
+            this.name = name;
+            this.eventId = eventId;
+        }
+    }
+}
