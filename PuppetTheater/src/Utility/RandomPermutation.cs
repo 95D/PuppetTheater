bfed333105dca31a176b00d8b641a1c305272e94
95D <avos170@gmail.com>
2020-05-04 13:57:26 +0900

    Add ObjectExtenstion, RandomPermutation, When classes

---
+++ PuppetTheater/src/Utility/RandomPermutation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Viento.PuppetTheater.Utiltiy
+{
+    public class RandomPermutation
+    {
+        private readonly Random random;
+
+        public RandomPermutation(Random random)
+        {
+            this.random = random;
+        }
+
+        public virtual ImmutableList<int> Next(int minimum, int maximum)
+        {
+            List<int> list = new List<int>();
+            if (minimum == maximum)
+                list.Add(minimum);
+            else
+                AddIndexRecursive(minimum, maximum-1, list);
+
+            return ImmutableList.Create(list.ToArray());
+        }
+
+        private void AddIndexRecursive(int minimum, int maximum, List<int> list)
+        {
+            int next = random.Next(minimum, maximum);
+            list.Add(next);
+            if(next + 1 <= maximum)
+            {
+                AddIndexRecursive(next + 1, maximum, list);
+            }
+            if(next - 1 >= minimum)
+            {
+                AddIndexRecursive(minimum, next - 1, list);
+            }
+        }
+    }
+}
---
+++ PuppetTheater/src/Utility/When.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Viento.PuppetTheater.Utiltiy
+{
+    using CaseTuple = Tuple<Func<bool>, Func<object>>;
+    public class BaseWhen<T>
+    {
+        protected readonly T real;
+        protected bool isComplete = false;
+        protected object answer = null;
+        
+        protected List<CaseTuple> cases = new List<CaseTuple>();
+        
+        protected BaseWhen(T real)
+        {
+            this.real = real;
+        }
+
+        public object End(Func<object> defaultCase = null)
+        {
+            foreach(var c in cases)
+            {
+                if(c.Item1())
+                {
+                    answer = c.Item2();
+                    isComplete = true;
+                    break;
+                }
+            }
+
+            if (isComplete)
+                return answer;
+            else if (defaultCase != null)
+                return defaultCase();
+            else
+                return null;
+        }
+    }
+
+    public sealed class When<T> : BaseWhen<T>
+    {
+        private readonly Func<T, T, bool> equals = (e, r) => e.Equals(r);
+
+        private When(T real) : base(real) { }
+
+        private When(T real, Func<T, T, bool> equals) : base(real) 
+        {
+            this.equals = equals;
+        }
+
+        public When<T> Case(T expected, Func<object> result)
+        {
+            cases.Add(new CaseTuple(
+                () => equals(expected, real), 
+                result
+                ));
+            return this;
+        }
+
+        public When<T> CaseThenAs<CAST>(T expected, Func<CAST, object> result)
+        {
+            cases.Add(new CaseTuple(
+                () => equals(expected, real),
+                () => real.AsDoing(result)
+                ));
+            return this;
+        }
+
+        public When<T> CaseRun(T expected, Action result)
+        {
+            cases.Add(new CaseTuple(
+                () => equals(expected, real),
+                () =>
+                {
+                    result();
+                    return null;
+                }));
+            return this;
+        }
+
+        public When<T> CaseThenAsRun<CAST>(T expected, Action<CAST> result)
+        {
+            cases.Add(new CaseTuple(
+                () => equals(expected, real),
+                () => real.AsDoing<CAST, object>(x =>
+                {
+                    result(x);
+                    return null;
+                })));
+            return this;
+        }
+
+        public static When<T> Start(T real)
+        {
+            return new When<T>(real);
+        }
+
+        public static When<T> Start(T real, Func<T, T, bool> equals)
+        {
+            return new When<T>(real, equals);
+        }
+    }
+
+    sealed class When : BaseWhen<object>
+    {
+        private When() : base(null) {}
+
+        public When Case(Func<bool> condition, Func<object> result)
+        {
+            cases.Add(new CaseTuple(
+                condition, result
+                ));
+            return this;
+        }
+
+        public When CaseRun(Func<bool> condition, Action result)
+        {
+            cases.Add(new CaseTuple(
+                condition,
+                () =>
+                {
+                    result();
+                    return null;
+                }));
+            return this;
+        }
+    }
+}
