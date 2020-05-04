cf54d75aa983584298559b62df8cc946064ae959
95D <avos170@gmail.com>
2020-05-04 13:48:58 +0900

    Add Select Node and Entity classes

---
+++ PuppetTheater/src/Node/Composite/SelectNode.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Viento.PuppetTheater.Base;
+
+namespace Viento.PuppetTheater.Node
+{
+    /// <summary>
+    /// <para>This class is Behavior Node which composite children.</para> 
+    /// <para>This class iterates executing child until child return true.</para>
+    /// </summary>
+    public class SelectNode : CompositeBaseNode
+    {
+        public SelectNode(
+            string name,
+            List<IBehaviorNode> children
+            ) : base(name, children)
+        {
+        }
+
+        protected override bool OnExecute(BehaviorContext context)
+        {
+            foreach (var child in children)
+            {
+                if (child.Execute(context))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ PuppetTheater/src/Serialization/Composite/SelectEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Viento.PuppetTheater.Serialization
+{
+    /// <summary>
+    /// This class data entity class for SelectNode.
+    /// </summary>
+    public class SelectEntity
+    {
+        public readonly string name;
+        public readonly List<string> children;
+
+        public SelectEntity(string name, List<string> children)
+        {
+            this.name = name;
+            this.children = children;
+        }
+    }
+}
