8027c96b983b9ee75d635f49e2bc33009b1b6fa6
95D <avos170@gmail.com>
2020-05-04 12:01:15 +0900

    Add FocusNode and FocusEntity classes

---
+++ PuppetTheater/src/Node/Action/FocusNode.cs
@@ -0,0 +1,31 @@
+﻿using Viento.PuppetTheater.API;
+using Viento.PuppetTheater.Base;
+
+namespace Viento.PuppetTheater.Node
+{
+    /// <summary>
+    /// This class is BehaviorNode for changing agent's focus.
+    /// </summary>
+    public class FocusNode : BehaviorNode
+    {
+        public readonly OnFocus focusOn;
+        public readonly int count;
+
+        public FocusNode(
+            string name,
+            OnFocus onFocus,
+            int count
+            ) : base(name)
+        {
+            this.focusOn = onFocus;
+            this.count = count;
+        }
+
+        protected override bool OnExecute(BehaviorContext context)
+        {
+            var immutableList = focusOn(count);
+            context.agentChannel.FocusTargets(context.agentId, immutableList);
+            return true;
+        }
+    }
+}
---
+++ PuppetTheater/src/Serialization/Action/FocusEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehaviorTree.Serialization.Action
+{
+    /// <summary>
+    /// This class data entity class for FocusNode.
+    /// </summary>
+    public class FocusEntity
+    {
+        public readonly string name;
+        public readonly string filterId;
+        public readonly string priorityId;
+        public readonly int count = 1;
+
+        public FocusEntity(
+            string name,
+            string filterId,
+            string priorityId,
+            int count
+            )
+        {
+            this.name = name;
+            this.filterId = filterId;
+            this.priorityId = priorityId;
+            this.count = count;
+        }
+    }
+}
