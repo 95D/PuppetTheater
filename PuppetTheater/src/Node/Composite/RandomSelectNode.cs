f42ffddb2cf6c914088b203faf67de5cb0972356
95D <avos170@gmail.com>
2020-05-04 13:48:35 +0900

    Add RandomSelect Node and Entity classes

---
+++ PuppetTheater/src/Node/Composite/RandomSelectNode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Viento.PuppetTheater.Base;
+using Viento.PuppetTheater.Utiltiy;
+
+namespace Viento.PuppetTheater.Node
+{
+    /// <summary>
+    /// <para>This class is Behavior Node which composite children.</para> 
+    /// <para>This class iterates executing child with random until child return true.</para>
+    /// </summary>
+    public class RandomSelectNode : CompositeBaseNode
+    {
+        private readonly RandomPermutation randomPermutation;
+
+        public RandomSelectNode(
+            string name,
+            List<IBehaviorNode> children
+            ) : base(name, children)
+        {
+            this.randomPermutation = new RandomPermutation(new Random());
+        }
+
+        public RandomSelectNode(
+            string name,
+            List<IBehaviorNode> children,
+            RandomPermutation randomPermutation
+            ) : base(name, children)
+        {
+            this.randomPermutation = randomPermutation;
+        }
+
+        protected override bool OnExecute(BehaviorContext context)
+        {
+            var indices = randomPermutation.Next(0, children.Count);
+            
+            foreach(int index in indices)
+            {
+                if (children[index].Execute(context))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ PuppetTheater/src/Serialization/Composite/RandomSelectEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Viento.PuppetTheater.Serialization
+{
+    /// <summary>
+    /// This class data entity class for RandomSelectNode.
+    /// </summary>
+    public class RandomSelectEntity
+    {
+        public readonly string name;
+        public readonly List<string> children;
+
+        public RandomSelectEntity(string name, List<string> children)
+        {
+            this.name = name;
+            this.children = children;
+        }
+    }
+}
