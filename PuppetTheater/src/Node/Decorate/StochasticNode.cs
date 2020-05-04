f7f7bc7ba2459b5a03d11b7245061a3168f77f0a
95D <avos170@gmail.com>
2020-05-04 13:56:39 +0900

    Add Stochastic Node and Entity classes

---
+++ PuppetTheater/src/Node/Decorate/StochasticNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Viento.PuppetTheater.Base;
+
+namespace Viento.PuppetTheater.Node
+{
+    /// <summary>
+    /// This class is BehaviorNode for processing child according to stochastic activation.
+    /// </summary>
+    public class StochasticNode : BehaviorNode
+    {
+        public readonly float threshold;
+        public readonly IBehaviorNode child;
+
+        public StochasticNode(
+            string name,
+            IBehaviorNode child,
+            float threshold
+            ) : base(name)
+        {
+            this.child = child;
+            this.threshold = threshold;
+        }
+
+        protected override bool OnExecute(BehaviorContext context)
+        {
+            var randGen = new Random();
+            if (randGen.NextDouble() > threshold)
+                return child.Execute(context);
+            else
+                return false;
+        }
+    }
+}
---
+++ PuppetTheater/src/Serialization/Decorate/StochasticEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace Viento.PuppetTheater.Serialization
+{
+    /// <summary>
+    /// This class data entity class for StochasticNode.
+    /// </summary>
+    public class StochasticEntity
+    {
+        public readonly string name;
+        public readonly float threshold;
+        public readonly string child;
+
+        public StochasticEntity(string name, float threshold, string child)
+        {
+            this.name = name;
+            this.threshold = threshold;
+            this.child = child;
+        }
+    }
+}
