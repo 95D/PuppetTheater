29f0854ea75fb54f5b68263a1ec6760830679fa8
95D <avos170@gmail.com>
2020-05-04 13:56:15 +0900

    Add Iterate Node and Entity classes

---
+++ PuppetTheater/src/Node/Decorate/IterateNode.cs
@@ -0,0 +1,32 @@
+﻿using Viento.PuppetTheater.Base;
+
+namespace Viento.PuppetTheater.Node
+{
+    /// <summary>
+    /// This class is BehaviorNode for processing child with 'range' times.
+    /// </summary>
+    public class IterateNode : BehaviorNode
+    {
+        public readonly int range;
+        public readonly IBehaviorNode child;
+
+        public IterateNode(
+            string name,
+            IBehaviorNode child,
+            int range
+            ) : base(name)
+        {
+            this.child = child;
+            this.range = range;
+        }
+
+        protected override bool OnExecute(BehaviorContext context)
+        {
+            for(int i = 0; i < range; i++)
+            {
+                child.Execute(context);
+            }
+            return true;
+        }
+    }
+}
---
+++ PuppetTheater/src/Serialization/Decorate/IterateEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace Viento.PuppetTheater.Serialization
+{
+    /// <summary>
+    /// This class data entity class for IterateNode.
+    /// </summary>
+    public class IterateEntity
+    {
+        public readonly string name;
+        public readonly int range;
+        public readonly string child;
+
+        public IterateEntity(string name, int range, string child)
+        {
+            this.name = name;
+            this.range = range;
+            this.child = child;
+        }
+    }
+}
