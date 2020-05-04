aea4135ce675a046fc2fbf79ba5abaf5e1478921
95D <avos170@gmail.com>
2020-05-04 13:53:22 +0900

    Add IfElse Node and Entity classes

---
+++ PuppetTheater/src/Node/Condition/IfElseNode.cs
@@ -0,0 +1,34 @@
+﻿using Viento.PuppetTheater.API;
+using Viento.PuppetTheater.Base;
+
+namespace Viento.PuppetTheater.Node
+{
+    /// <summary>
+    /// This class is BehaviorNode for processing child then or else according to condition.
+    /// </summary>
+    public class IfElseNode : ConditionBaseNode
+    {
+        public readonly IBehaviorNode thenChild;
+        public readonly IBehaviorNode elseChild;
+
+        public IfElseNode(
+            string name,
+            OnCheckCondition onCheckCondition,
+            IBehaviorNode thenChild, 
+            IBehaviorNode elseChild
+            ) : base(name, onCheckCondition)
+        {
+            this.thenChild = thenChild;
+            this.elseChild = elseChild;
+        }
+
+        protected override bool OnExecute(BehaviorContext context)
+        {
+            if (inference(context))
+                return thenChild.Execute(context);
+            else
+                return elseChild.Execute(context);
+
+        }
+    }
+}
---
+++ PuppetTheater/src/Serialization/Condition/IfElseEntity.cs
@@ -0,0 +1,26 @@
+﻿namespace Viento.PuppetTheater.Serialization
+{
+    /// <summary>
+    /// This class data entity class for IfElseNode.
+    /// </summary>
+    public class IfElseEntity
+    {
+        public readonly string name;
+        public readonly string conditionFuncName;
+        public readonly string thenChild;
+        public readonly string elseChild;
+
+        public IfElseEntity(
+            string name,
+            string conditionFuncName,
+            string thenChild,
+            string elseChild
+            )
+        {
+            this.name = name;
+            this.conditionFuncName = conditionFuncName;
+            this.thenChild = thenChild;
+            this.elseChild = elseChild;
+        }
+    }
+}
