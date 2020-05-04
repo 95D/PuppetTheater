ffec8d376dc465edd478d936d4fa0ebf843048cb
95D <avos170@gmail.com>
2020-05-04 13:49:29 +0900

    Add Sequence Node and Entity classes

---
+++ PuppetTheater/src/Serialization/Composite/SequenceEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Viento.PuppetTheater.Serialization
+{
+    /// <summary>
+    /// This class data entity class for SeqenceNode.
+    /// </summary>
+    public class SequenceEntity
+    {
+        public readonly string name;
+        public readonly List<string> children;
+
+        public SequenceEntity(string name, List<string> children)
+        {
+            this.name = name;
+            this.children = children;
+        }
+    }
+}
