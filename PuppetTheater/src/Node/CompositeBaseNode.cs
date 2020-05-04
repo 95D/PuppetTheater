9a3c95ecf9c4378f75db14b2cf10dcb081e0f588
95D <avos170@gmail.com>
2020-05-04 13:48:01 +0900

    Add CompositeBaseNode class

---
+++ PuppetTheater/src/Node/CompositeBaseNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Viento.PuppetTheater.Node
+{
+    public abstract class CompositeBaseNode : BehaviorNode
+    {
+        public readonly List<IBehaviorNode> children;
+
+        public CompositeBaseNode(
+            string name,
+            List<IBehaviorNode> children
+            ) : base(name)
+        {
+            this.children = children;
+        }
+    }
+}
