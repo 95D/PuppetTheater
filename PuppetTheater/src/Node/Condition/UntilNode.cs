3a4621d152951c988dad5825d6ce4f9debdc5600
95D <avos170@gmail.com>
2020-05-04 13:54:14 +0900

    Add Until Node and Entity classes

---
+++ PuppetTheater/src/Node/Condition/UntilNode.cs
@@ -0,0 +1,33 @@
+﻿using Viento.PuppetTheater.API;
+using Viento.PuppetTheater.Base;
+
+namespace Viento.PuppetTheater.Node
+{
+    /// <summary>
+    /// This class is BehaviorNode for processing child until condition is false.
+    /// </summary>
+    public class UntilNode : ConditionBaseNode
+    {
+        public readonly IBehaviorNode untilChild;
+
+        public UntilNode(
+            string name,
+            OnCheckCondition onCheckCondition,
+            IBehaviorNode thenChild
+            ) : base(name, onCheckCondition)
+        {
+            this.untilChild = thenChild;
+        }
+
+        protected override bool OnExecute(BehaviorContext context)
+        {
+            bool isSuccess = false;
+            while(inference(context))
+            {
+                untilChild.Execute(context);
+                isSuccess = true;
+            }
+            return isSuccess;
+        }
+    }
+}
---
+++ PuppetTheater/src/Serialization/Condition/UntilEntity.cs
@@ -0,0 +1,23 @@
+﻿namespace Viento.PuppetTheater.Serialization
+{
+    /// <summary>
+    /// This class data entity class for UntilNode.
+    /// </summary>
+    public class UntilEntity
+    {
+        public readonly string name;
+        public readonly string conditionFuncName;
+        public readonly string thenChild;
+
+        public UntilEntity(
+            string name,
+            string conditionFuncName,
+            string thenChild
+            )
+        {
+            this.name = name;
+            this.conditionFuncName = conditionFuncName;
+            this.thenChild = thenChild;
+        }
+    }
+}
