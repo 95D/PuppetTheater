abdbce21ec870b3edc02e824c06fb372f790dd12
95D <avos170@gmail.com>
2020-05-05 00:17:47 +0900

    Add Configuration, BehaviorTreeRoot classes

---
+++ PuppetTheater/src/Base/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Viento.PuppetTheater.Base
+{
+    public class Configuration
+    {
+        public readonly bool isTimeLimit;
+        public readonly float timeout;
+        public readonly bool isHopLimit;
+        public readonly int hopout;
+
+        public static Configuration CreateInstance(string json)
+        {
+            return JsonConvert.DeserializeObject(json) as Configuration;
+        }
+    }
+}
