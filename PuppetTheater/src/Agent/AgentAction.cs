f9c069fab1a56e3dada758b0c0787fc359a17821
95D <avos170@gmail.com>
2020-05-04 11:20:27 +0900

    Add AgentAction class

---
+++ PuppetTheater/src/Agent/AgentAction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Immutable;
+
+namespace Viento.PuppetTheater.Agent
+{
+    public class AgentAction
+    {
+        public readonly ImmutableList<int> targets;
+        public readonly string eventId;
+
+        public AgentAction(ImmutableList<int> targets, string eventId)
+        {
+            this.targets = targets;
+            this.eventId = eventId;
+        }
+    }
+}
