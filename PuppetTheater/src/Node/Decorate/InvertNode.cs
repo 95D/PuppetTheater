31f1c958c086e6b643bda80042ffa23681dfd326
95D <avos170@gmail.com>
2020-05-04 13:55:51 +0900

    Add Invert Node and Entity classes

---
+++ PuppetTheater/src/Node/Decorate/InvertNode.cs
@@ -0,0 +1,25 @@
+﻿using Viento.PuppetTheater.Base;
+
+namespace Viento.PuppetTheater.Node
+{
+    /// <summary>
+    /// This class is BehaviorNode for processing child and do invert return value
+    /// </summary>
+    public class InvertNode : BehaviorNode
+    {
+        public readonly IBehaviorNode child;
+
+        public InvertNode(
+            string name,
+            IBehaviorNode child
+            ) : base(name)
+        {
+            this.child = child;
+        }
+
+        protected override bool OnExecute(BehaviorContext context)
+        {
+            return !child.Execute(context);
+        }
+    }
+}
