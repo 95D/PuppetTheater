0ddcf407d4a6964649ee2ba408c3f072364259a3
95D <avos170@gmail.com>
2020-05-04 11:43:49 +0900

    Add CancelBehaviorException class

---
+++ PuppetTheater/src/Exception/CancelBehaviorException.cs
@@ -0,0 +1,24 @@
+﻿using Viento.PuppetTheater.Base;
+
+namespace Viento.PuppetTheater.Exception
+{
+    /// <summary>
+    /// This class is exception class which occur at canceling behavior tree process. 
+    /// </summary>
+    public class CancelBehaviorException : System.Exception
+    { 
+        public enum Cause
+        {
+            Command = 0,
+            Timeout = 1,
+            Hopout = 2
+        }
+
+        public CancelBehaviorException(
+            BehaviorContext context,
+            Cause cause
+            ) : base("Behavior tree is cancelled. CAUSE:[" + cause.ToString() + "].\n" +
+                context.GetStackTrace())
+        { }
+    }
+}
