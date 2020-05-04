f7e3092fb3f0e0a921ac79fda3ed0a925e162653
95D <avos170@gmail.com>
2020-05-04 11:54:06 +0900

    Add BehaviorNode class

---
+++ PuppetTheater/src/Node/BehaviorNode.cs
@@ -0,0 +1,32 @@
+﻿using Viento.PuppetTheater.Base;
+
+namespace Viento.PuppetTheater.Node
+{
+    /// <summary>
+    /// This class is a unit of the Behavior tree.
+    /// </summary>
+    public abstract class BehaviorNode : IBehaviorNode
+    {
+        private readonly string _behaviorId;
+        public virtual string behaviorId { get => _behaviorId ; }
+        public BehaviorNode(string behaviorId)
+        {
+            this._behaviorId = behaviorId;
+        }
+
+        public bool Execute(BehaviorContext context)
+        {
+            context.OnExecuteStart(behaviorId);
+            bool result = OnExecute(context);
+            context.OnExecuteEnd();
+            return result;
+        }
+
+        protected abstract bool OnExecute(BehaviorContext context);
+
+        public override int GetHashCode()
+        {
+            return behaviorId.GetHashCode();
+        }
+    }
+}
---
+++ PuppetTheater/src/Node/IBehaviorNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Viento.PuppetTheater.Base;
+
+namespace Viento.PuppetTheater
+{
+    /// <summary>
+    /// This interface is for the BehaviorNode class.
+    /// </summary>
+    public interface IBehaviorNode
+    {
+        string behaviorId { get; }
+        bool Execute(BehaviorContext context);
+    }
+}
