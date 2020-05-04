b03b3b57c16f0b4c372e8f36ce477c38914f9078
95D <avos170@gmail.com>
2020-05-04 13:53:46 +0900

    Add If Node and Entity classes

---
+++ PuppetTheater/src/Node/Condition/IfNode.cs
@@ -0,0 +1,31 @@
+﻿using Viento.PuppetTheater.API;
+using Viento.PuppetTheater.Base;
+
+namespace Viento.PuppetTheater.Node
+{
+    /// <summary>
+    /// This class is BehaviorNode for processing child then according to condition.
+    /// </summary>
+    public class IfNode : ConditionBaseNode
+    {
+
+        public readonly IBehaviorNode thenChild;
+
+        public IfNode(
+            string name,
+            OnCheckCondition onCheckCondition,
+            IBehaviorNode thenChild
+            ) : base(name, onCheckCondition)
+        {
+            this.thenChild = thenChild;
+        }
+
+        protected override bool OnExecute(BehaviorContext context)
+        {
+            if (inference(context))
+                return thenChild.Execute(context);
+            else
+                return false;
+        }
+    }
+}
---
+++ PuppetTheater/src/Serialization/Condition/IfEntity.cs
@@ -0,0 +1,23 @@
+﻿namespace Viento.PuppetTheater.Serialization
+{
+    /// <summary>
+    /// This class data entity class for IfNode
+    /// </summary>
+    public class IfEntity
+    {
+        public readonly string name;
+        public readonly string conditionFuncName;
+        public readonly string thenChild;
+
+        public IfEntity(
+            string name,
+            string conditionFuncName,
+            string thenChild
+            )
+        {
+            this.name = name;
+            this.conditionFuncName = conditionFuncName;
+            this.thenChild = thenChild;
+        }
+    }
+}
