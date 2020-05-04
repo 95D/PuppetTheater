c439cb76ad0cb21d42c913ed73f22850d2fb9427
95D <avos170@gmail.com>
2020-05-04 11:20:40 +0900

    Add Agent class

---
+++ PuppetTheater/src/Agent/Agent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Viento.PuppetTheater.Agent
+{
+    public class Agent
+    {
+        public readonly int agentId;
+        readonly Queue<AgentAction> actionQueue;
+
+        public ImmutableList<int> currentTargets;
+
+        public void Publish(string eventId)
+        {
+            actionQueue.Enqueue(new AgentAction(currentTargets, eventId));
+        }
+    }
+}
