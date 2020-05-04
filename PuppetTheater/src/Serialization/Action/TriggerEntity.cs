cf77c5626a42101a2ff8e1f6b2f6fc827ff30410
95D <avos170@gmail.com>
2020-05-05 00:13:53 +0900

    Fix namespace Action, Focus, Trigger Entity

--- PuppetTheater/src/Serialization/Action/TriggerEntity.cs
+++ PuppetTheater/src/Serialization/Action/TriggerEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BehaviorTree.Serialization.Action
+﻿namespace Viento.PuppetTheater.Serialization
 {
     /// <summary>
     /// This class data entity class for TriggerNode.
