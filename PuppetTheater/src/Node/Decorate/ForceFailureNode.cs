d8c5a9fc266ecf4ba477a268dd5840619f2f46c0
95D <avos170@gmail.com>
2020-05-04 13:55:26 +0900

    Add ForceFailure Node and Entity classes

---
+++ PuppetTheater/src/Node/Decorate/ForceFailureNode.cs
@@ -0,0 +1,26 @@
+﻿using Viento.PuppetTheater.Base;
+
+namespace Viento.PuppetTheater.Node
+{
+    /// <summary>
+    /// This class is BehaviorNode for processing child and always return false.
+    /// </summary>
+    public class ForceFailureNode : BehaviorNode
+    {
+        public readonly IBehaviorNode child;
+
+        public ForceFailureNode(
+            string name,
+            IBehaviorNode child
+            ) : base(name)
+        {
+            this.child = child;
+        }
+
+        protected override bool OnExecute(BehaviorContext context)
+        {
+            child.Execute(context);
+            return false;
+        }
+    }
+}
---
+++ PuppetTheater/src/Serialization/Decorate/ForceFailureEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace Viento.PuppetTheater.Serialization
+{
+    /// <summary>
+    /// This class data entity class for ForceFailureNode.
+    /// </summary>
+    public class ForceFailureEntity
+    {
+        public readonly string name;
+        public readonly string child;
+
+        public ForceFailureEntity(string name, string child)
+        {
+            this.name = name;
+            this.child = child;
+        }
+    }
+}
