c13eecdb23a754ddfa1ae7de2bd7a613033b3866
95D <avos170@gmail.com>
2020-05-04 13:49:54 +0900

    Add ConditionBaseNode class

---
+++ PuppetTheater/src/Node/ConditionBaseNode.cs
@@ -0,0 +1,17 @@
+﻿using Viento.PuppetTheater.API;
+
+namespace Viento.PuppetTheater.Node
+{
+    public abstract class ConditionBaseNode : BehaviorNode
+    {
+        public readonly OnCheckCondition inference;
+        
+        internal ConditionBaseNode(
+            string name,
+            OnCheckCondition onConditionCheck
+            ) : base(name)
+        {
+            this.inference = onConditionCheck;
+        }
+    }
+}
