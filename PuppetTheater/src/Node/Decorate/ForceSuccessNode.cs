3ec130e96a76c99d44108396c669e8a79ef97a5a
95D <avos170@gmail.com>
2020-05-04 13:54:58 +0900

    Add ForceSuccess Node and Entity classes

---
+++ PuppetTheater/src/Node/Decorate/ForceSuccessNode.cs
@@ -0,0 +1,26 @@
+﻿using Viento.PuppetTheater.Base;
+
+namespace Viento.PuppetTheater.Node
+{
+    /// <summary>
+    /// This class is BehaviorNode for processing child and always return true.
+    /// </summary>
+    public class ForceSuccessNode : BehaviorNode
+    {
+        public readonly IBehaviorNode child;
+
+        public ForceSuccessNode(
+            string name,
+            IBehaviorNode child
+            ) : base(name)
+        {
+            this.child = child;
+        }
+
+        protected override bool OnExecute(BehaviorContext context)
+        {
+            child.Execute(context);
+            return true;
+        }
+    }
+}
---
+++ PuppetTheater/src/Serialization/Decorate/ForceSuccessEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace Viento.PuppetTheater.Serialization
+{
+    /// <summary>
+    /// This class data entity class for ForceSuccessNode.
+    /// </summary>
+    public class ForceSuccessEntity
+    {
+        public readonly string name;
+        public readonly string child;
+
+        public ForceSuccessEntity(string name, string child)
+        {
+            this.name = name;
+            this.child = child;
+        }
+    }
+}
