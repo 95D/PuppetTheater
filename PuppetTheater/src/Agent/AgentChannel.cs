d391a29ff9e9c8d1b03ac8aa30a3041f81b0ad6c
95D <avos170@gmail.com>
2020-05-04 11:20:56 +0900

    Add AgentChannel class

---
+++ PuppetTheater/src/Agent/AgentChannel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Viento.PuppetTheater.Agent
+{
+    /// <summary>
+    /// This class collects agent instances and publishes event from behavior tree.
+    /// </summary>
+    public class AgentChannel
+    {
+        Dictionary<int, Agent> agentMap;
+
+        public void PutAgent(Agent agent)
+        {
+            agentMap.Add(agent.agentId.GetHashCode(), agent);
+        }
+        
+        public void FocusTargets(string agentId, ImmutableList<int> targets)
+        {
+            agentMap[agentId.GetHashCode()].currentTargets = targets;
+        }
+
+        public void PublishAction(string agentId, string eventId)
+        {
+            var agent = agentMap[agentId.GetHashCode()];
+            agent.actionQueue.Enqueue(new AgentAction(agent.currentTargets, eventId));
+        }
+
+        public bool HasAction(string agentId)
+
+        public AgentAction Subscribe(string agentId)
+        {
+
+        }
+    }
+}
