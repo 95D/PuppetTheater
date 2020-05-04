3029d3ab1a90d13dc22e698679a5166e9f92034e
95D <avos170@gmail.com>
2020-05-04 12:01:49 +0900

    Add TriggerNode and TriggerEntity classes

---
+++ PuppetTheater/src/Node/Action/TriggerNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Viento.PuppetTheater.Base;
+
+namespace Viento.PuppetTheater.Node
+{
+    /// <summary>
+    /// This class is BehaviorNode for publishing action for specific agent.
+    /// </summary>
+    public class TriggerNode : BehaviorNode
+    {
+        public readonly string agentId;
+        public readonly string eventId;
+        public TriggerNode(
+            string name, 
+            string agentId, 
+            string eventId
+            ) : base(name)
+        {
+            this.agentId = agentId;
+            this.eventId = eventId;
+        }
+
+        protected override bool OnExecute(BehaviorContext context)
+        {
+            context.agentChannel.PublishAction(agentId, eventId);
+            return true;
+        }
+    }
+}
---
+++ PuppetTheater/src/Serialization/Action/TriggerEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehaviorTree.Serialization.Action
+{
+    /// <summary>
+    /// This class data entity class for TriggerNode.
+    /// </summary>
+    public class TriggerEntity
+    {
+        public readonly string name;
+        public readonly string eventId;
+        public readonly string agentId;
+
+        public TriggerEntity(
+            string name,
+            string eventId,
+            string agentId
+            )
+        {
+            this.name = name;
+            this.eventId = eventId;
+            this.agentId = agentId;
+        }
+    }
+}
