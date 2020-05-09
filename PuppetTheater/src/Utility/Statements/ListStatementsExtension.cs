5bb8b6a6da6dbafd984f7a7d811937e38f9269a1
95D <avos170@gmail.com>
2020-05-09 23:42:41 +0900

    Add statement extension functions

---
+++ PuppetTheater/src/Utility/Statements/ListStatementsExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Text;
+
+namespace Viento.PuppetTheater.Utility
+{
+    public static class ListStatementsExtension
+    {
+        public static string ToStringChildren(this IList list)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < list.Count; i++)
+                if (i == 0)
+                    sb.Append(list[i].ToString());
+                else
+                    sb.Append(", " + list[i].ToString());
+            return sb.ToString();
+        }
+    }
+}
---
+++ PuppetTheater/src/Utility/Statements/TypeStatementsExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PuppetTheater.src.Utility.Statements
+{
+    public static class TypeStatementsExtension
+    {
+        public static bool IsList(this Type type)
+        {
+            return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>);
+        }
+    }
+}
