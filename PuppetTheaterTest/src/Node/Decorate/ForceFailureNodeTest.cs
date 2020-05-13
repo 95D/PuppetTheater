7b8118ea627fe4ef0d2238fc1b6950551028bb92
95D <avos170@gmail.com>
2020-05-13 21:50:27 +0900

    Add test code for ForceFailureNodeTest

---
+++ PuppetTheaterTest/src/Node/Decorate/ForceFailureNodeTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Viento.PuppetTheater.Agent;
+using Viento.PuppetTheater.API;
+using Viento.PuppetTheater.Base;
+using Viento.PuppetTheater.Node;
+
+namespace Viento.PuppetTheaterTest.Node
+{
+    [TestClass]
+    public class ForceFailureNodeTest
+    {
+        private Mock<BlackBoard> blackBoard;
+        private Mock<AgentChannel> agentChannel;
+        private Mock<BehaviorContext> bContext;
+        private Mock<IBehaviorNode> successNode;
+        private Mock<IBehaviorNode> failureNode;
+
+        [TestInitialize]
+        public void TestUp()
+        {
+            blackBoard = new Mock<BlackBoard>();
+            agentChannel = new Mock<AgentChannel>();
+            bContext = new Mock<BehaviorContext>(
+                blackBoard.Object,
+                agentChannel.Object,
+                "testBehaviorContext",
+                /* isTimeLimit */ false, /* timeout */ 0,
+                /* isHopLimit */ false, /* hopout */ 0
+            );
+            successNode = new Mock<IBehaviorNode>();
+            failureNode = new Mock<IBehaviorNode>();
+
+            successNode.Setup(x => x.GetHashCode()).Returns("SuccessNode".GetHashCode());
+            successNode.Setup(x => x.Execute(bContext.Object)).Returns(true);
+            failureNode.Setup(x => x.GetHashCode()).Returns("FailureNode".GetHashCode());
+            failureNode.Setup(x => x.Execute(bContext.Object)).Returns(false);
+        }
+
+        [TestMethod]
+        public void test_success()
+        {
+            var testNode = new ForceFailureNode(
+                "testNode",
+                successNode.Object);
+
+            Assert.AreEqual(false, testNode.Execute(bContext.Object));
+
+            successNode.Verify(x => x.Execute(bContext.Object));
+        }
+
+        [TestMethod]
+        public void test_failure()
+        {
+            var testNode = new ForceFailureNode(
+                "testNode",
+                failureNode.Object);
+
+            Assert.AreEqual(false, testNode.Execute(bContext.Object));
+
+            failureNode.Verify(x => x.Execute(bContext.Object));
+        }
+    }
+}
