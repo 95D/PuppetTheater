297b6cef66fd0926a9a7609a94978891d044a26c
95D <avos170@gmail.com>
2022-02-03 14:15:04 +0900

    Address formatting

--- PuppetTheater/src/Node/Composite/PermutateCategory.cs
+++ PuppetTheater/src/Node/Composite/PermutateCategory.cs
@@ -1,8 +1,10 @@
-namespace Viento.PuppetTheater.Node {
+namespace Viento.PuppetTheater.Node
+{
     /// <summary>
     /// A enum for classifying how [CompisteNode] permutates children 
     /// </summary>
-    public enum PermutateCategory {
+    public enum PermutateCategory
+    {
         Ascending,
         OrderedRandom,
         InfiniteRandom
