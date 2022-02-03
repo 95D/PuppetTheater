a024720a3b39c2ba2a59c6c007d3d0a08dc42895
95D <avos170@gmail.com>
2022-02-03 14:15:29 +0900

    Add method

--- PuppetTheater/src/Node/Composite/CompositeCategory.cs
+++ PuppetTheater/src/Node/Composite/CompositeCategory.cs
@@ -15,14 +15,29 @@ namespace Viento.PuppetTheater.Node
         /// <summary>
         /// Select category: Iterate until child's result is succeeded 
         /// </summary>
-        public static CompositeCategory Select = new CompositeCategory(
+        public static CompositeCategory select = new CompositeCategory(
                 finallyState: NodeLifeCycle.Failed,
                 breakState: NodeLifeCycle.Success);
 
         /// <summary>
         /// Sequnece category: Iterate until child's result is failed 
         /// </summary>
-        public static CompositeCategory Sequence =
+        public static CompositeCategory sequence =
             new CompositeCategory(finallyState: NodeLifeCycle.Success, breakState: NodeLifeCycle.Failed);
+
+        private const string KEY_SELECT = "select";
+        private const string KEY_SEQUENCE = "sequence";
+        public static CompositeCategory From(string key)
+        {
+            switch (key)
+            {
+                case KEY_SELECT:
+                    return select;
+                case KEY_SEQUENCE:
+                    return sequence;
+            }
+            throw new System.ApplicationException(
+                string.Format("The given key for category {0} is not supported!", key));
+        }
     }
 }
