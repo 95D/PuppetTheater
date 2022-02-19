1eb75b486b6cdad0e76cccb3b16b9c7204a4ffa7
95D <avos170@gmail.com>
2022-02-19 10:22:55 +0900

    Fix incorrect spec for StochasticNode

--- PuppetTheater/src/Node/Decorate/StochasticNode.cs
+++ PuppetTheater/src/Node/Decorate/StochasticNode.cs
@@ -9,42 +9,45 @@ namespace Viento.PuppetTheater.Node
     public sealed class StochasticNode : DecorateNode
     {
         public readonly float threshold;
+        private readonly Random random;
 
         public StochasticNode(
             string nodeId,
             BehaviorNode child,
-            float threshold
-            ) : base(nodeId, child)
+            float threshold) : this(nodeId, child, threshold, new Random())
         {
-            this.threshold = threshold;
         }
 
-        private TraversalState InvokeByThreshold(TraversalState traversalState, bool isSucceeded)
+        public StochasticNode(
+            string nodeId,
+            BehaviorNode child,
+            float threshold,
+            Random random) : base(nodeId, child)
         {
-            var randGen = new Random();
-            if (randGen.NextDouble() > threshold)
-            {
-                return traversalState.UpdateCurrentNodeLifeCycle(NodeLifeCycle.Running)
-                    .PushNode(child.CreateNodeStateAsReady());
-            }
-            else if (isSucceeded)
-                return traversalState.UpdateCurrentNodeLifeCycle(NodeLifeCycle.Success);
-            else
-                return traversalState.UpdateCurrentNodeLifeCycle(NodeLifeCycle.Failed);
+            this.threshold = threshold;
+            this.random = random;
         }
 
         public override TraversalState TraverseDown(
             string puppetId,
             IPuppetController puppetController,
             TraversalState traversalState,
-            long currentMillis) => InvokeByThreshold(traversalState, false);
+            long currentMillis)
+        {
+            if (random.NextDouble() > threshold)
+            {
+                return traversalState.UpdateCurrentNodeLifeCycle(NodeLifeCycle.Running)
+                    .PushNode(child.CreateNodeStateAsReady());
+            }
+            else
+                return traversalState.UpdateCurrentNodeLifeCycle(NodeLifeCycle.Failed);
+        }
 
         public override TraversalState TraverseUp(
             string puppetId,
             IPuppetController puppetController,
             TraversalState traversalState,
-            NodeState childNodeState) => InvokeByThreshold(
-                traversalState,
-                childNodeState.lifeCycle.isSucceeded());
+            NodeState childNodeState) => traversalState
+                .UpdateCurrentNodeLifeCycle(childNodeState.lifeCycle);
     }
 }
